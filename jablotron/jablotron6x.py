2050cb0636bfff4da6c66ef0a3894ad4b393f448
xandys <154770152+xandys@users.noreply.github.com>
2024-04-14 14:02:09 +0200

    Update jablotron6x.py
    Converted to python 3

--- jablotron/jablotron6x.py
+++ jablotron/jablotron6x.py
@@ -3,7 +3,6 @@ import serial
 import sys
 import collections
 import logging
-from itertools import izip
 from time import sleep
 
 
@@ -22,9 +21,9 @@ KEY_MAP = {
         '8': 0x88,
         '9': 0x89,
         'N': 0x8E,
-        'F': 0x8F, 
+        'F': 0x8F,
     }
-KEY_MAP_INVERSED = {v: k for k, v in KEY_MAP.items()}
+KEY_MAP_INVERSED = {v: k for k, v in list(KEY_MAP.items())}
 
 # how the codes send in e0 event map to letters/symbols on display
 DISPLAY_MAP = {
@@ -32,17 +31,17 @@ DISPLAY_MAP = {
         0x06: ' 6', 0x07: ' 7', 0x08: ' 8', 0x09: ' 9', 0x0A: '10',
         0x0B: '11', 0x0C: '12', 0x0D: '13', 0x0E: '14', 0x0F: '15',
         0x10: '16',
-        0x11: ' A',             0x13: ' C', 0x14: ' d', 
+        0x11: ' A',             0x13: ' C', 0x14: ' d',
                     0x17: ' U',             0x19: '  ', 0x1A: ' P',
         0x1B: ' -', 0x1C: ' L', 0x1D: ' J', 0x1E: '| ', 0x1F: ' |',
         0x21: 'c1', 0x22: 'c2', 0x23: 'c3', 0x24: 'c4', 0x25: 'c5',
-        0x26: 'c6', 0x27: 'c7', 0x28: 'c8',             
+        0x26: 'c6', 0x27: 'c7', 0x28: 'c8',
 }
 
 # led masks for e0
 LED_MAP = {
         0x01: 'power',
-        0x02: 'alarm',               
+        0x02: 'alarm',
         0x04: 'tamper',
         #0x07: 'malfunction',
         0x10: 'lock',
@@ -52,7 +51,7 @@ LED_MAP = {
 
 # modes for e0
 MODE_MAP = {
-	0x00: 'service mode',
+        0x00: 'service mode',
         0x20: 'user mode',
         0x40: 'disarmed',
         0x41: 'armed',
@@ -102,7 +101,7 @@ class Jablotron6x(object):
         """
         Instantiates the class
 
-        :param device: string containing the name of the serial 
+        :param device: string containing the name of the serial
                        device where alarm is connected to.
         """
         self._device = device
@@ -110,19 +109,19 @@ class Jablotron6x(object):
         self._callbacks = []
 
         self._con = serial.Serial(
-            baudrate=9600, 
+            baudrate=9600,
             parity=serial.PARITY_NONE,
             bytesize=serial.EIGHTBITS,
             dsrdtr=True,
-            timeout=0.1) 
-	self._con.port = self._device
+            timeout=0.1)
+        self._con.port = self._device
 
-	self._register_internal_callbacks()
+        self._register_internal_callbacks()
 
     def _register_internal_callbacks(self):
-	# 0x8? events
-	for mask in KEY_MAP.values():
-		self.register_callback(self._handle_on_key_press, [mask])
+        # 0x8? events
+        for mask in list(KEY_MAP.values()):
+            self.register_callback(self._handle_on_key_press, [mask])
         # 0xe0 status events
         self.register_callback(self._handle_status_update, [0xe0])
 
@@ -148,9 +147,9 @@ class Jablotron6x(object):
 
         :return: self
         """
-	if self._con.is_open:
-        	self._con.flush()
-        	self._con.close()
+        if self._con.is_open:
+            self._con.flush()
+            self._con.close()
         return self
 
     def send(self, buf):
@@ -160,9 +159,9 @@ class Jablotron6x(object):
         :param buf: list of numbers/bytes
         :return: self
         """
-	for b in buf:
-	    self._con.write(chr(b))
-	self._con.flush()
+        for b in buf:
+            self._con.write(chr(b))
+        self._con.flush()
         return self
 
     def send_keys(self, keys):
@@ -170,37 +169,37 @@ class Jablotron6x(object):
         Send key presses to alarm.
 
         :param keys: string containing characters 0..9 + F + N
-        :return: self 
+        :return: self
         """
-	try:
+        try:
           for k in keys:
             self.send([KEY_MAP[k]])
             sleep(0.1)
-	except KeyError:
-	  raise ValueError("Invalid Keys: %s" % keys)
-	return self
+        except KeyError:
+          raise ValueError("Invalid Keys: %s" % keys)
+        return self
 
     def register_callback(self, callback, mask=[]):
         """
         Register callback for received event.
 
-        :param mask: list of bytes that must match the beginning of 
+        :param mask: list of bytes that must match the beginning of
                      the alarm message.
                      For info about jablotron messages see the wiki:
-		     https://github.com/pezinek/py-jablotron6x/wiki/Protocol
+                     https://github.com/pezinek/py-jablotron6x/wiki/Protocol
         :param callback: callback with following signature
 
                 my_callback(buf)
                 :param buf: list of bytes received
-                :return:  EVENT_CONSUMED or EVENT_NOT_CONSUMED, 
+                :return:  EVENT_CONSUMED or EVENT_NOT_CONSUMED,
                           based on this following callbacks will
                           or will not be executed.
 
         :return: self
         """
 
-	c = _callback(function=callback, mask=mask)
-	self._callbacks.append(c)
+        c = _callback(function=callback, mask=mask)
+        self._callbacks.append(c)
         return self
 
 
@@ -209,16 +208,16 @@ class Jablotron6x(object):
 
         for cb in self._callbacks:
             match = True
-            for b, c in izip(buf, cb.mask):
+            for b, c in zip(buf, cb.mask):
                 if c is None:
                     continue
                 if b != c:
                     match = False
                     break;
             if match:
-                consume_event = cb.function(buf) 
-		if consume_event:
-			return
+                consume_event = cb.function(buf)
+                if consume_event:
+                    return
 
     def loop(self):
         """
@@ -228,27 +227,27 @@ class Jablotron6x(object):
         """
 
         #process all pending chars
-	while True:
-		if not self._con.is_open:
-			break
+        while True:
+                if not self._con.is_open:
+                        break
 
-		b = self._con.read()
-		if len(b) == 0:
-			break
+                b = self._con.read()
+                if len(b) == 0:
+                        break
 
-		b = ord(b)
-        	self._read_buffer.append(b)
+                b = ord(b)
+                self._read_buffer.append(b)
 
-        	if (b == 0xFF):
-            		self._handle_event(self._read_buffer)
-	    		self._read_buffer = []
+                if (b == 0xFF):
+                            self._handle_event(self._read_buffer)
+                            self._read_buffer = []
 
-	return self
+        return self
 
-    def loop_forever(self): 
+    def loop_forever(self):
         """Process data from serial line forever."""
-	while True:
-	    self.loop()
+        while True:
+            self.loop()
 
 
     #
@@ -258,17 +257,17 @@ class Jablotron6x(object):
     # on_key_press
 
     def _handle_on_key_press(self, buf):
-	for b in buf:	
-		try:
-			key = KEY_MAP_INVERSED[b]
-			self.on_key_press(key)	
-		except TypeError:
-			# no callback defined
-			pass
-		except KeyError:
-			# invalid key (unkown 0x8? code)
-			pass
-	return EVENT_NOT_CONSUMED
+        for b in buf:
+                try:
+                        key = KEY_MAP_INVERSED[b]
+                        self.on_key_press(key)
+                except TypeError:
+                        # no callback defined
+                        pass
+                except KeyError:
+                        # invalid key (unkown 0x8? code)
+                        pass
+        return EVENT_NOT_CONSUMED
 
 
     @property
@@ -279,48 +278,48 @@ class Jablotron6x(object):
        Expected signature of the callback is:
          key_press_callback(key)
 
-       key:	characters 1-9 or N or F
+       key: characters 1-9 or N or F
        """
        return self._on_key_press
 
     @on_key_press.setter
     def on_key_press(self, func):
-      """ Define the key pres callback implementation. 
+      """ Define the key pres callback implementation.
 
       Expected signature is:
          key_press_callback(key)
 
-      key:	characters 1-9 or N or F
+      key: characters 1-9 or N or F
 
       """
       self._on_key_press=func
 
 
-    # status update 
- 
+    # status update
+
     def _handle_status_update(self, buf):
-   	if buf[1] != self.mode:
+        if buf[1] != self.mode:
           self.mode = buf[1]
-	  try:
+          try:
             text = MODE_MAP[self.mode]
           except KeyError:
             logging.error("MODE_MAP does not contain value for 0x%02x" % self.mode)
-	    text = "Mode 0x%02x" % self.mode
+            text = "Mode 0x%02x" % self.mode
 
           try:
             self.on_mode_change(text)
           except TypeError:
             # no mode callback defined
-	    pass
+            pass
 
         if buf[2] != self.leds:
           args={}
-          for mask, name in LED_MAP.items():
+          for mask, name in list(LED_MAP.items()):
              new = buf[2] & mask
              old = self.leds & mask
              if new != old:
                args[name]=bool(new)
-         
+
           self.leds = buf[2]
           try:
             self.on_led_change(**args)
@@ -331,8 +330,8 @@ class Jablotron6x(object):
         if buf[3] != self.display:
           self.display = buf[3]
 
-	  try:
-	    text = DISPLAY_MAP[self.display & 0b01111111]
+          try:
+            text = DISPLAY_MAP[self.display & 0b01111111]
           except KeyError:
             logging.error("DISPLAY_MAP does not contain value for 0x%02x" % self.display)
             text = "0x%02x" % self.display
@@ -344,7 +343,7 @@ class Jablotron6x(object):
             pass
 
 
-	return EVENT_NOT_CONSUMED
+        return EVENT_NOT_CONSUMED
 
 
     @property
@@ -404,7 +403,7 @@ class Jablotron6x(object):
 
 def print_buffer(buf):
     """ callback for printing raw event buffer. """
-    print("%s" % " ".join(["%02x" % c for c in buf]))
+    print(("%s" % " ".join(["%02x" % c for c in buf])))
     return EVENT_NOT_CONSUMED
 
 def consume_event(buf):
@@ -415,7 +414,7 @@ def remove_duplicities(buf):
     """ callback for discarding duplicate events. """
     checksum = buf[len(buf)-2]
     if remove_duplicities.last == checksum:
-	return EVENT_CONSUMED
+        return EVENT_CONSUMED
     remove_duplicities.last = checksum
     return EVENT_NOT_CONSUMED
 
@@ -429,6 +428,5 @@ if __name__ == "__main__":
         #j.register_callback([], print_all).send_keys('NNN').loop_forever()
         #j.register_callback(consume_event, mask=[0xe0])
         j.register_callback(remove_duplicities, mask=[0xe0])
-	j.register_callback(print_buffer)
-	j.loop_forever()
-
+    j.register_callback(print_buffer)
+    j.loop_forever()
